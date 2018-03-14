f4ac9106b524edb96dd17e873990c7c0ac941156
Jacob Czech <czech.jacob@gmail.com>
2018-03-14 17:19:26 -0400

    Add templates.

--- ingest/views.py
+++ ingest/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from .models import MinimalImgMetadata
+
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    latest_metadata_list = MinimalImgMetadata.objects.order_by('-organization_name')[:5]
+    context = {'latest_metadata_list': latest_metadata_list}
+    return render(request, 'ingest/index.html', context)
 
 def detail(request, metadata_id):
-    return HttpResponse("You're looking at metadata %s." % metadata_id)
+    metadata = get_object_or_404(MinimalImgMetadata, pk=metadata_id)
+    return render(request, 'ingest/detail.html', {'metadata': metadata})
