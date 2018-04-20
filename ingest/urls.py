f6c1b36c12fc73621dc2d6d0f3c5daa3905478f7
Jacob Czech <czech.jacob@gmail.com>
2018-04-20 17:06:42 -0400

    Add some really preliminary account creation code.

--- ingest/urls.py
+++ ingest/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.conf.urls import url
 
 from . import views
 
@@ -7,4 +8,6 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('new/', views.post_new, name='post_new'),
+    url(r'^signup/$', views.signup, name='signup')
+    #path('signup/', views.signup, name='signup')
 ]
--- ingest/views.py
+++ ingest/views.py
@@ -3,9 +3,27 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
 from django.shortcuts import redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 from .models import MinimalImgMetadata
 from .forms import MIMForm
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
 
 
 class IndexView(generic.ListView):
