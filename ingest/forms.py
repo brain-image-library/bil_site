af0ae36d3015d447a70449e3e733ecd1f5e2fb6d
Jacob Czech <czech.jacob@gmail.com>
2018-04-23 17:55:49 -0400

    Ask for additional fields to create account.

--- ingest/forms.py
+++ ingest/forms.py
@@ -1,6 +1,18 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 from .models import MinimalImgMetadata
+
+
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=False, help_text='Required')
+    last_name = forms.CharField(max_length=30, required=False, help_text='Required')
+    email = forms.EmailField(max_length=254, help_text='Required. A valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
 
 class MIMForm(forms.ModelForm):
 
--- ingest/views.py
+++ ingest/views.py
@@ -9,20 +9,20 @@ from django.shortcuts import render, redirect
 
 from .models import MinimalImgMetadata
 from .forms import MIMForm
-
+from .forms import SignUpForm
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('../')
+            return redirect('..')
     else:
-        form = UserCreationForm()
+        form = SignUpForm()
     return render(request, 'ingest/signup.html', {'form': form})
 
 
