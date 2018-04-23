9835584514c05ce59942120327d5fc4006619a3f
Jacob Czech <czech.jacob@gmail.com>
2018-04-23 18:17:05 -0400

    Make first and last name required.

--- ingest/forms.py
+++ ingest/forms.py
@@ -6,8 +6,8 @@ from .models import MinimalImgMetadata
 
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False, help_text='Required')
-    last_name = forms.CharField(max_length=30, required=False, help_text='Required')
+    first_name = forms.CharField(max_length=30, help_text='Required')
+    last_name = forms.CharField(max_length=30, help_text='Required')
     email = forms.EmailField(max_length=254, help_text='Required. A valid email address.')
 
     class Meta:
