1ab2c3b41e459f05051a41a56d0556ecc111a6e5
Jacob Czech <czech.jacob@gmail.com>
2018-04-23 17:42:42 -0400

    Move the signup template with the others.
    Also, redirect to a valid URL after submission of form.

--- ingest/views.py
+++ ingest/views.py
@@ -20,10 +20,10 @@ def signup(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('home')
+            return redirect('../')
     else:
         form = UserCreationForm()
-    return render(request, 'signup.html', {'form': form})
+    return render(request, 'ingest/signup.html', {'form': form})
 
 
 class IndexView(generic.ListView):
