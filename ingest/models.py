f5bb82befd2ca16486ff948ab45b07bededfb0ce
Jacob Czech <czech.jacob@gmail.com>
2018-04-24 15:43:00 -0400

    Use choices for organization

--- ingest/models.py
+++ ingest/models.py
@@ -4,7 +4,19 @@ from django.db import models
 class MinimalImgMetadata(models.Model):
     def __str__(self):
         return self.project_name
-    organization_name = models.CharField(max_length=200)
+    AI = 'AI'
+    CSHL = 'CSHL'
+    USC = 'USC'
+    ORGANIZATION_CHOICES = (
+        (CSHL, 'Cold Spring Harbor Laboratory'),
+        (USC, 'University of Southern California'),
+        (AI, 'Allen Institute'),
+    )
+    organization_name = models.CharField(
+        max_length=200,
+        choices=ORGANIZATION_CHOICES,
+        default=AI,
+    )
     project_name = models.CharField(max_length=200)
     project_description = models.CharField(max_length=200)
     project_funder_id = models.CharField(max_length=200)
