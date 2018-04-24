21e9812e1705e64c0dda201030c9913f819a4d66
Jacob Czech <czech.jacob@gmail.com>
2018-04-24 11:39:05 -0400

    Add login and logout pages.
    Currently, login redirects to a page that doesn't exist.

--- ingest/urls.py
+++ ingest/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
 from . import views
 
@@ -8,6 +9,8 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('new/', views.post_new, name='post_new'),
-    url(r'^signup/$', views.signup, name='signup')
-    #path('signup/', views.signup, name='signup')
+    url(r'^signup/$', views.signup, name='signup'),
+    url(r'^login/$', auth_views.login, {'template_name': 'ingest/login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'template_name': 'ingest/logged_out.html'}, name='logout'),
+
 ]
