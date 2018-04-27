8d047b173800e1f5a6b0fceb2aa242592e8c71fe
Jacob Czech <czech.jacob@gmail.com>
2018-04-27 15:33:11 -0400

    Be more consistent about urlpatterns.
    Also, use re_path instead of url, since django docs says that it will
    likely be deprecated in the future.

--- ingest/urls.py
+++ ingest/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.conf.urls import url
+from django.conf.urls import re_path
 from django.contrib.auth import views as auth_views
 
 from . import views
@@ -7,10 +7,10 @@ from . import views
 app_name = 'ingest'
 urlpatterns = [
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('new/', views.post_new, name='post_new'),
-    url(r'^signup/$', views.signup, name='signup'),
-    url(r'^login/$', auth_views.login, {'template_name': 'ingest/login.html'}, name='login'),
-    url(r'^logout/$', auth_views.logout, {'template_name': 'ingest/logged_out.html'}, name='logout'),
-    url(r'^index/$', views.index, name='index'),
-    url(r'^metadata_list/$', views.metadata_list, name='metadata_list'),
+    re_path(r'^new/$', views.post_new, name='post_new'),
+    re_path(r'^signup/$', views.signup, name='signup'),
+    re_path(r'^login/$', auth_views.login, {'template_name': 'ingest/login.html'}, name='login'),
+    re_path(r'^logout/$', auth_views.logout, {'template_name': 'ingest/logged_out.html'}, name='logout'),
+    re_path(r'^index/$', views.index, name='index'),
+    re_path(r'^metadata_list/$', views.metadata_list, name='metadata_list'),
 ]
