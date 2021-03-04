d0b0a54fe393fdff96c5d2cc89e9da1cb4bd621e
Liz Pantalone <ecp@psc.edu>
2021-03-04 10:42:32 -0500

    updating sendemail.js file with Juan's code

--- ingest/static/ingest/sendemail.js
+++ ingest/static/ingest/sendemail.js
@@ -1,41 +1,18 @@
 function collection_send() {
-  debugger;
-  const rowform = document.getElementById("rowform")
-  const all_submit_for_validations = document.getElementById("submit_for_validation")
-  const all_rows = rowform.elements["row"]
-  const all_names = rowform.elements["name"]
-  const all_descriptions = rowform.elements["description"]
-  const all_organization_labs = rowform.elements["organization_lab"]
-  const all_lab_names = rowform.elements["lab_name"]
-  const all_project_funder_ids = rowform.elements["project_funder_id"]
-  const all_project_funders = rowform.elements["project_funder"]
-  const all_bil_uuids = rowform.elements["bil_uuid"]
-  const all_data_path = rowform.elements["data_path"]
-  const all_locked = rowform.elements["locked"]
-  const all_submission_statuses = rowform.elements["submission_status"]
-  const all_validation_statuses = rowform.elements["validation_status"]
-  let output_rows = []
-  for(let i = 0; i < all_names.length; i++)
-  {
-      const rowdata = {
-          rowid: all_rows[i].value,
-          submit_for_validation: all_submit_for_validations[i].value, 
-          name: all_names[i].value,
-          description: all_descriptions[i].value,
-          organization_lab: all_organization_labs[i].value,
-          lab_name: all_lab_names[i].value,
-          project_funder_id: all_project_funder_ids[i].value,
-          project_funder: all_project_funders[i].value,
-          bil_uuid: all_bil_uuids[i].value,
-          data_path: all_data_path[i].value,
-          locked: all_locked[i].value,
-          submission_status: all_submission_statuses[i].value,
-          validation_status: all_validation_statuses[i].value,
-      };
-      output_rows.push(rowdata);
-  }
-  console.log(output_rows);
-  fetch('${window.origin}/collection_send', {
+    let output_rows = []
+    $('tbody>tr').each(function(i, e){
+        let $e = $(e);
+        // $e is the tr element
+        let row_is_checked = $e.find('#collection_is_checked').is(':checked');
+        // row_is_checked is a boolean of whether that row is checked or not
+        let bil_uuid = $e.find('#bil')
+        if(row_is_checked)
+            output_rows.push({
+                'billUuid': bil_uuid
+                }
+            )
+    });
+  fetch(window.origin + '/collection_send', {
       method: "POST",
       credentials: "include",
       body: JSON.stringify(output_rows),
@@ -52,11 +29,9 @@ function collection_send() {
               response.json().then(function(data) {
               console.log(data);
               window.location.replace(data['url']);
-          });  
+          });
       })
       .catch(function(error) {
           console.log("Fetch error: " + error);
-   
   });
 } 
-
