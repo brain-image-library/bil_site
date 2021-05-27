af18dd6069f07d5e987a9cc16b3d160e3b43c8f4
Luke Tuite <ltuite96@gmail.com>
2021-05-27 11:03:56 -0400

    users table renders, need to fix things so it populates

--- ingest/views.py
+++ ingest/views.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.contrib import messages
 from django.contrib import auth
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.files.storage import FileSystemStorage
@@ -75,7 +76,6 @@ def index(request):
     allprojectpeople = ProjectPeople.objects.all()
     try:
         if project_person.is_bil_admin:
-            print("we got to the right place")
             return render(request, 'ingest/bil_index.html', {'project_person': project_person})#, {'allpeople': allpeople}, {'allprojectpeople': allprojectpeople})
         elif project_person.is_pi:
             return render(request, 'ingest/pi_index.html', {'project_person': project_person})
@@ -85,6 +85,49 @@ def index(request):
 
 # What follows is a number of views for uploading, creating, viewing, modifying
 # and deleting IMAGE METADATA.
+def manageUsers(request):
+    """ The main/home page. """
+    current_user = request.user
+    allusers = User.objects.all()
+    print(allusers)
+    people = People.objects.get(auth_user_id_id = current_user.id)
+    project_person = ProjectPeople.objects.get(people_id = people.id)
+    allpeople = People.objects.all()
+    allprojectpeople = ProjectPeople.objects.all()
+    try:
+        if project_person.is_bil_admin:
+            print('this is in the if statement')
+            return render(request, 'ingest/manage_users.html', {'project_person': project_person, 'allpeople': allpeople, 'allprojectpeople': allprojectpeople, 'allusers':allusers})
+       
+    except Exception as e:
+        print(e)
+        return render(request, 'ingest/index.html')
+
+class CollectionList(LoginRequiredMixin, SingleTableMixin, FilterView):
+    """ A list of all a user's collections. """
+
+    table_class = CollectionTable
+    model = Collection
+    template_name = 'ingest/collection_list.html'
+    filterset_class = CollectionFilter
+
+    def get_queryset(self, **kwargs):
+        return Collection.objects.filter(user=self.request.user)
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        context['collections'] = Collection.objects.filter(user=self.request.user)
+        return context
+
+    def get_filterset_kwargs(self, filterset_class):
+        """ Sets the default collection filter status. """
+        kwargs = super().get_filterset_kwargs(filterset_class)
+        if kwargs["data"] is None:
+            kwargs["data"] = {"submit_status": "NOT_SUBMITTED"}
+        return kwargs
+
+
 
 
 @login_required
