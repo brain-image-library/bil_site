2879d14297e0af390cfef113a826b8b8ba816198
Liz Pantalone <ecpantalone@gmail.com>
2021-06-24 11:27:29 -0400

    javascript has been debugged and is moving data for the project form. need to debug backend

--- ingest/static/ingest/createproject.js
+++ ingest/static/ingest/createproject.js
@@ -1,42 +1,42 @@
 function create_new_project() {
     const csrftoken = Cookies.get('csrftoken');
-    let output_rows = [];
-    //const newprojectform = document.getElementByID("CreateNewProject");
-	   
-    let name = $e.find('#name')
-    let is_biccn = $e.find('#is_biccn')
-    let funded_by = $e.find('#funded_by')
+    const output_rows = [];
     
+    const name = document.getElementById("name");
+    const is_biccn = document.getElementById("is_biccn");
+    const funded_my = document.getElementById("is_biccn");
+
     output_rows.push({
         "name": name.value,
         "is_biccn": is_biccn.value,
         "funded_by": funded_by.value
         })
-  }
-    //console.log(output_rows)
-        
+  
+    console.log(output_rows)
+     
                 
             
   fetch(`${window.origin}/ingest/create_project/`, {
-      method: "POST",
-      credentials: "include",
-      body: JSON.stringify(output_rows),
-      cache: "no-cache",
-      headers: new Headers({
-          "X-CSRFToken": csrftoken,
-          "content-type": "application/json"
-      })
-  })
-      .then(function(response) {
-          if (response.status !== 200) {
-              console.log('Looks like there was a problem. Status code: ${response.status}');
-              return;
-          }
-              response.json().then(function(data) {
-              console.log(data);
-              window.location.replace(data['url']);
-          });
-      })
-      .catch(function(error) {
-          console.log("Fetch error: " + error);
-      });
+       method: "POST",
+       credentials: "include",
+       body: JSON.stringify(output_rows),
+       cache: "no-cache",
+       headers: new Headers({
+           "X-CSRFToken": csrftoken,
+           "content-type": "application/json"
+       })
+   })
+       .then(function(response) {
+           if (response.status !== 200) {
+               console.log('Looks like there was a problem. Status code: ${response.status}');
+               return;
+           }
+               response.json().then(function(data) {
+               console.log(data);
+               window.location.replace(data['url']);
+           });
+       })
+       .catch(function(error) {
+           console.log("Fetch error: " + error);
+       });
+}
