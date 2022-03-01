2d74b4b8a0b09435d72f86549b1c7ef88d888809
Liz Pantalone <ecpantalone@gmail.com>
2022-02-28 19:23:05 -0500

    adding js file

---
+++ ingest/static/ingest/metadataloadingindicator.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+  //ajax code here (example for $.post) using test page from https://reqres.in
+  //Adding a delay so we can see the functionality of the loader while request processes
+  $("#UploadMetadata").on("click", function(e) {
+    e.preventDefault();
+    $("#loadMe").modal({
+      backdrop: "static", //remove ability to close modal with click
+      keyboard: false, //remove option to close with keyboard
+      show: true //Display loader!
+    });
+    var url = "${window.origin}/ingest/descriptive_metadata_upload/";
+    $.post(
+      url,
+      function(response) {
+        if (response.data[0]) {
+          //if you received a successful return, remove the modal. Either way remove the modal!!
+          var resOutput =
+            '<h4 style="color: black">Success!</h4>';
+          $("#output").html(resOutput);
+          $("#loadMe").modal("hide");
+        } else {
+          $("#output").html(
+            '<div class="alert alert-warning"><h4>Please see the errors or contact BIL Support</h4></div>'
+          );
+        }
+      },
+      "json"
+    );
+  });
+});
