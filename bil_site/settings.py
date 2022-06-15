a395ad447331201127d08958a50dd4463893d181
Liz Pantalone <ecpantalone@gmail.com>
2022-06-15 11:51:29 -0400

    updating example config to reflect email vars

--- bil_site/settings.py
+++ bil_site/settings.py
@@ -214,8 +214,8 @@ MESSAGE_TAGS = {
 #Email Settings
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
-EMAIL_HOST_USER = 'CHANGE_ME'
-EMAIL_HOST_PASSWORD = 'CHANGE_ME'
+EMAIL_HOST_USER = config['Security']['DATABASE']['EMAIL_USER']
+EMAIL_HOST_PASSWORD = config['Security']['DATABASE']['EMAIL_PASSWORD']
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 #EMAIL_USE_SSL = False
